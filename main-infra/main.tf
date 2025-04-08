18c8eb442032bc2698971d859ef3739cd41c9293
NoriTN <nori.tran-nguyen@ext.saint-gobain.com>
2025-04-08 15:56:11 +0200

    a

--- main-infra/main.tf
+++ main-infra/main.tf
@@ -70,10 +70,10 @@ module "app_service" {
   source = "../app-service"
 
   for_each = {
-    for web_app in var.web_app : "${module.naming_app_service.resource_name}-${web_app.suffix}" => web_app
+    for web_app in var.web_app : "${module.naming_app_service.resource_name}-${var.web_app.suffix}" => web_app
   }
 
-  name = "${module.naming_app_service.resource_name}-${web_app.suffix}"
+  name = "${module.naming_app_service.resource_name}-${var.web_app.suffix}"
   resource_group_name = module.resource_group.name
   location           = local.location
   app_name           = var.app_name
