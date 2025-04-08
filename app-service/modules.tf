379fda450698da8c5481a6451732bc225b050a3e
NoriTN <nori.tran-nguyen@ext.saint-gobain.com>
2025-04-08 16:03:28 +0200

    a

--- app-service/modules.tf
+++ app-service/modules.tf
@@ -2,6 +2,6 @@ module "naming_app_service_plan" {
   source        = "../naming"
   app_name      = var.app_name
   environment   = var.environment
-  suffix        = var.suffix
+  suffix        = ""
   resource_type = "appsp"
 }
