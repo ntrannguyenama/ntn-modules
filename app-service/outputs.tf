f6d211af918671358ec98762ea3c5fb2d3e52e22
NoriTN <nori.tran-nguyen@ext.saint-gobain.com>
2025-04-08 14:58:39 +0200

    a

--- app-service/outputs.tf
+++ app-service/outputs.tf
@@ -3,17 +3,17 @@ output "app_service_plan_id" {
   description = "The ID of the App Service Plan"
 }
 
-output "app_service_name" {
-  description = "The name of the App Service"
-  value       = azurerm_linux_web_app.main.name
-}
+# output "app_service_name" {
+#   description = "The name of the App Service"
+#   value       = azurerm_linux_web_app.main.name
+# }
 
-output "app_service_hostname" {
-  description = "The default hostname of the App Service"
-  value       = azurerm_linux_web_app.main.default_hostname
-}
+# output "app_service_hostname" {
+#   description = "The default hostname of the App Service"
+#   value       = azurerm_linux_web_app.main.default_hostname
+# }
 
-output "app_service_identity" {
-  description = "The identity of the App Service"
-  value       = azurerm_linux_web_app.main.identity[0].principal_id
-}
+# output "app_service_identity" {
+#   description = "The identity of the App Service"
+#   value       = azurerm_linux_web_app.main.identity[0].principal_id
+# }
