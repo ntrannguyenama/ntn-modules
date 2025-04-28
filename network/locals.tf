8e1526736b530b0cf26385810b744882d82f379c
NoriTN <nori.tran-nguyen@ext.saint-gobain.com>
2025-04-29 00:52:03 +0200

    i

--- network/locals.tf
+++ network/locals.tf
@@ -1,6 +1,6 @@
 locals {  
 virtual_network_private_dns_zone = {
-    for private_dns_zone in var.dns_zone : "${var.environment}-${private_dns_zone.name}" => {
+    for private_dns_zone in var.dns_zone : "${private_dns_zone.name}" => {
       name             = private_dns_zone.name
       private_dns_name = private_dns_zone.private_dns_name
     }
