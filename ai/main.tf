d9955229c41dbfec49fc0bc75cb671e53546d735
NoriTN <nori.tran-nguyen@ext.saint-gobain.com>
2025-05-02 14:10:48 +0200

    ai

--- ai/main.tf
+++ ai/main.tf
@@ -69,7 +69,7 @@ resource "azurerm_search_service" "main" {
   name                = module.naming_search.resource_name
   resource_group_name = var.resource_group_name
   location            = var.location
-  sku                = var.cognitive_search_sku
+  sku                = "Free"
   tags               = var.tags
 
   identity {
