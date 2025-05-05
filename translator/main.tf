a0f2082df58eec61a5979021e3b3c29e002b00c2
NoriTN <nori.tran-nguyen@ext.saint-gobain.com>
2025-05-05 13:14:23 +0200

    Update main.tf

--- translator/main.tf
+++ translator/main.tf
@@ -8,7 +8,7 @@ module "naming_translator" {
 }
 
 resource "azurerm_cognitive_account" "translator" {
-  name                = module.naming_translator
+  name                = module.naming_translator.resource_name
   location            = var.location
   resource_group_name = var.resource_group_name
   kind                = "TextTranslation"
