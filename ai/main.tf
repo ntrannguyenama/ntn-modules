da6935017ae303b036766774fd0b385866708551
NoriTN <nori.tran-nguyen@ext.saint-gobain.com>
2025-05-05 09:44:52 +0200

    Update main.tf

--- ai/main.tf
+++ ai/main.tf
@@ -26,13 +26,13 @@ module "naming_search" {
 
 
 resource "azurerm_cognitive_account" "openai" {
-  name                = module.naming_openai.resource_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  name                = "scanbeton-api"
+  location            = "eastus2"
+  resource_group_name = "ai"
   kind                = "OpenAI"
   sku_name            = "S0"
   tags                = var.tags
-  custom_subdomain_name = module.naming_openai.resource_name
+  custom_subdomain_name = "scanbeton-api"
 
   dynamic_throttling_enabled = false
   fqdns = []
