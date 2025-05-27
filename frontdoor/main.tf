64b542e9772be865f95950fb03aedf4d924fbc6f
NoriTN <ntrannguyen@amayas.fr>
2025-05-27 15:16:38 +0200

    fix q

--- frontdoor/main.tf
+++ frontdoor/main.tf
@@ -20,39 +20,39 @@ resource "azurerm_cdn_frontdoor_profile" "frontdoor" {
 # }
 
 # # Define the Origin Group (equivalent to backend pool)
-# resource "azurerm_cdn_frontdoor_origin_group" "origin_group" {
-#   name                     = "webapp-backend"
-#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.frontdoor.id
+resource "azurerm_cdn_frontdoor_origin_group" "origin_group" {
+  name                     = "webapp-backend"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.frontdoor.id
 
-#   # Load balancing settings
-#   load_balancing {
-#     sample_size                        = 4
-#     successful_samples_required         = 3
-#     additional_latency_in_milliseconds = 50 # Adjust as needed
-#   }
+  # Load balancing settings
+  load_balancing {
+    sample_size                        = 4
+    successful_samples_required         = 3
+    additional_latency_in_milliseconds = 50 # Adjust as needed
+  }
 
-#   # Health probe settings
-#   health_probe {
-#     path                = "/api"
-#     protocol            = "Http"
-#     interval_in_seconds = 100
-#     request_type        = "HEAD" # Optional: Use HEAD for lighter probes
-#   }
-# }
+  # Health probe settings
+  health_probe {
+    path                = "/api"
+    protocol            = "Http"
+    interval_in_seconds = 100
+    request_type        = "HEAD" # Optional: Use HEAD for lighter probes
+  }
+}
 
-# # Define the Origin (equivalent to backend)
-# resource "azurerm_cdn_frontdoor_origin" "origin" {
-#   name                           = "webapp-origin"
-#   cdn_frontdoor_origin_group_id  = azurerm_cdn_frontdoor_origin_group.origin_group.id
-#   enabled                        = true
-#   certificate_name_check_enabled = false
-#   host_name                      = "scanbetonama-dev-app.azurewebsites.net"
-#   origin_host_header             = "scanbetonama-dev-app.azurewebsites.net"
-#   http_port                      = 80
-#   https_port                     = 443
-#   priority                       = 1
-#   weight                         = 1000
-# }
+# Define the Origin (equivalent to backend)
+resource "azurerm_cdn_frontdoor_origin" "origin" {
+  name                           = "webapp-backend"
+  cdn_frontdoor_origin_group_id  = azurerm_cdn_frontdoor_origin_group.origin_group.id
+  enabled                        = true
+  certificate_name_check_enabled = false
+  host_name                      = "scanbetonama-dev-app.azurewebsites.net"
+  origin_host_header             = "scanbetonama-dev-app.azurewebsites.net"
+  http_port                      = 80
+  https_port                     = 443
+  priority                       = 1
+  weight                         = 1000
+}
 
 # # Define the Route (equivalent to routing rule)
 # resource "azurerm_cdn_frontdoor_route" "route" {
